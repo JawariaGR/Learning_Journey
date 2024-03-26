d4d9bb51f38a0151d49006412e6cdbe8801476c0
JAWARIA <160843181+JawariaGR@users.noreply.github.com>
2024-03-26 22:53:27 +0500

    19/45

---
+++ 45_question/Q18/Q18.js
@@ -0,0 +1,7 @@
+"use strict";
+// QUESTION NO 18 (ORGANIZED ARRAY IN DYNAMIC SITUATION)
+let place = ["China", "Australia", "Turkey", "Maldives", "Indonesia",];
+console.log("Original place", place);
+console.log("Alhapabtical Order", place.sort());
+console.log("Reverse Order", place.reverse());
+console.log("Reverse Alphabatical Order", place.sort().reverse());
---
+++ 45_question/Q18/Q18.ts
@@ -0,0 +1,7 @@
+// QUESTION NO 18 (ORGANIZED ARRAY IN DYNAMIC SITUATION)
+let place: string[] = ["China", "Australia","Turkey","Maldives", "Indonesia",];
+console.log("Original place", place);
+console.log("Alhapabtical Order", place.sort());
+console.log("Reverse Order", place.reverse());
+console.log("Reverse Alphabatical Order", place.sort().reverse());
+
