e59740136873298bfc2ae8d3e72e269c14d43860
JAWARIA <160843181+JawariaGR@users.noreply.github.com>
2024-04-03 01:15:57 +0500

    ADD ATM AND TO DO LIST

---
+++ online_class/to_do_list/index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what you want to add in your Todos?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false"
+        },
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
+let taskdelete = await inquirer.prompt([
+    {
+        name: "delete",
+        type: "input",
+        message: "which task to you want to delete?",
+    }
+]);
