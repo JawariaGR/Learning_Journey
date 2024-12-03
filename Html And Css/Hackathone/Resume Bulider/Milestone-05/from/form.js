cab5200ccc60d30f25e99a40ba999ffb65fe1850
JawariaGR <jawariaghulamrasool@gmail.com>
2024-12-03 21:47:33 +0500

    rename file name

--- Html And Css/Hackathone/Resume Bulider/Milestone-05/from/form.js
+++ Html And Css/Hackathone/Resume Bulider/Milestone-05/from/form.js
@@ -89,5 +89,5 @@ form?.addEventListener("submit", (e) => {
         });
         reader.readAsDataURL(Pic.files[0]);
     }
-    window.location.href = "Resume/index.html";
+    window.location.href = "/Resume/resume.html";
 });
